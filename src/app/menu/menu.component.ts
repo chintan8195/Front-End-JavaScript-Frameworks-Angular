2a354a7c5c1735a0220f74fcac9bf1ee8f0ca806
Chintan Parikh <cparikh@scu.edu>
2018-07-23 17:29:52 -0700

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,7 +11,7 @@ import { DISHES } from '../shared/dishes';
 export class MenuComponent implements OnInit {
   dishes: Dish[] = DISHES;
 
-  selectedDish = DISHES[0];
+  selectedDish:Dish ;
                       
                  
 
