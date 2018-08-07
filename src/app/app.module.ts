7c91fc2eb25b2e9b848e4a1f6129f5ffd26a4471
Chintan Parikh <cparikh@scu.edu>
2018-08-06 20:00:47 -0700

    Angular Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,17 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service'
 
 import { AppRoutingModule } from './app-routing/app-routing.module'
 
@@ -47,7 +50,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService , PromotionService],
+  providers: [DishService , PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
