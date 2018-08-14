536eeca69c74a28f5e12b1310562bf6a3396742d
Chintan Parikh <cparikh@scu.edu>
2018-08-14 13:09:26 -0700

    Angular RxJS Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
-import { Observable,of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
 
@@ -23,6 +23,10 @@ export class DishService {
 
   getFeaturedDish(): Observable<Dish> {
     return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
+  }
+
+  getDishIds(): Observable<number[] | any> {
+    return of(DISHES.map(dish => dish.id ));
   }
   
 }
