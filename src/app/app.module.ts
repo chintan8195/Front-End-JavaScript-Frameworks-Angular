cb7201b7562a7f8bd9a9adf19eea11bf2ae6a09a
Chintan Parikh <cparikh@scu.edu>
2018-08-18 11:32:26 -0700

    Angular HTTP Client Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,14 @@ import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatSliderModule} from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -29,13 +32,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { LoginComponent } from './login/login.component';
+import {baseURL} from './shared/baseurl';
 
 
 
@@ -66,14 +71,19 @@ import { LoginComponent } from './login/login.component';
     MatSlideToggleModule, 
     MatInputModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
     MatCheckboxModule,
+    HttpModule,
+    HttpClientModule,
     FormsModule, 
     MatDialogModule,
     ReactiveFormsModule
   ],
   providers: [  DishService,
     PromotionService,
-    LeaderService
+    LeaderService,
+    {provide:'BaseURL',useValue : baseURL},
+    ProcessHTTPMsgService
   ],
   entryComponents:[
     LoginComponent
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Inject  , ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment'
 import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dishdetail',
@@ -13,15 +13,35 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-
-  dish:  Dish;
+  dish : Dish;
   dishIds: number[];
-  prev:number;
-  next:number;
+  prev: number;
+  next: number;
+  commentForm: FormGroup;
+  comment: Comment;
 
+  @ViewChild('cform') commentFormDirective;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Author Name is required.',
+      'minlength':     'Author Name must be at least 2 characters long.',
+    },
+    'comment': {
+      'required':      'Comment is required.'
+    }
+  };
+  
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder,
+    @Inject('BaseURL') private BaseURL) { this.createForm();}
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -29,8 +49,57 @@ export class DishdetailComponent implements OnInit {
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
 
+  createForm():void {
+  this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)] ],
+      rating: 5 ,
+      comment: ['', [Validators.required] ],
+      date: new Date().toISOString()
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    console.log(this.comment);
+    
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      author: '',
+      rating: '5',
+      comment: ''
+    });
+    this.commentFormDirective.resetForm();
+    console.log(this.commentFormDirective.form.controls.rating.setValue(5));
+  }
+
   setPrevNext(dishId: number) {
-    const index = this.dishIds.indexOf(dishId);
+    let index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
     this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
